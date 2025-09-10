ef84dbc501aca34927999e251c18cea128922992
TynK-M <matteo.pauroso@gmail.com>
2025-09-10 23:10:53 +0200

    Fix: expand token Expanded the definition of a token by separating it in TokenType and Token Feat: create lexer struct Created the skeleton for the lexer structure

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,7 @@
-/// Represents the different kinds of tokens in the language.
+/// Represents the different types of tokens in the language.
 /// These are used by the lexer to categorize pieces of input.
 #[derive(Debug, PartialEq)]
-enum Token {
+enum TokenType {
     Keyword(String),        // e.g., "def"
     Identifier(String),     // e.g., "header", "font-style"
     StrinLiteral(String),   // e.g., "Hello, World!"
@@ -13,4 +13,22 @@ enum Token {
     LBrace, RBrace,         // e.g., '{' and '}'
     LessThan, GreaterThan,  // e.g., '<' and '>'
     EOF,                    // End of input
+}
+
+/// Represents a single token
+/// 
+/// Each token has a `value` (the raw String from the input)
+/// and a `token_type` that categorizes its role in the language.
+#[derive(Debug, Clone, PartialEq)]
+struct Token {
+    value: String,
+    token_type: TokenType,
+}
+
+/// The Lexer struct is responsible for scanning the input string
+/// and producing a stream of tokens for the parser to consume.
+struct Lexer {
+    input: String,                  // The input source code as String
+    position: usize,                // The current position in the input
+    current_char: Option<char>,     // The character that is currently being examined
 }
